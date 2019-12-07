8b6d096495f695b15ab6f6297fe5cc1e89701f71
heasun0111 <48648940+heasun0111@users.noreply.github.com>
2019-12-08 03:31:39 +0900

    장애물,Goal 인식 구현

--- project_matrix/project_matrix.ino
+++ project_matrix/project_matrix.ino
@@ -164,6 +164,62 @@ void turn_right(int x, int y) {
 }
 
 
+//장애물 인지
+void detect_hurdle(int x, int y){
+  long duration, length;
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(100);
+  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  long cm = microsecondsToCentimeters(duration);
+  delay(500);
+  
+  if (cm < 10)   //if문 안에 들어갈 조건은 한 칸의 간격으로 조정한다.
+  {
+    servoLeft.writeMicroseconds(1500);
+    servoRight.writeMicroseconds(1500);
+    if (오른쪽 이동){
+      arr[x+1][y] = 2;
+    }
+    if (왼쪽 이동){
+      arr[x-1][y] = 2;
+    }
+    if (위쪽 이동){
+      arr[x][y+1] = 2;
+    }
+    if (아래쪽 이동){
+      arr[x][y-1] = 2;
+    }
+  }
+  
+}
+
+//Goal 인지
+void detect_Goal(int x, int y){
+  if ( state_M < black && state_L >= black && state_R >= black)
+  {
+    servoLeft.writeMicroseconds(1500);
+    servoRight.writeMicroseconds(1500);
+    
+    if (오른쪽 이동){
+      arr[x+1][y] = 3;
+    }
+    if (왼쪽 이동){
+      arr[x-1][y] = 3;
+    }
+    //Goal의 경우 문제 조건에 의해서 위,아래로 움직일 때 만날 일이 없다.
+  }
+  
+}
+
+//장애물,Goal을 인지한 후 장애물을 돌아가는 함수
+void avoid_obstacle (int x, int y){
+  
+}
+
 void loop() {
 
 }
